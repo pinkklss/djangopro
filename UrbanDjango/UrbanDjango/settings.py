7b10a60d51c2f7b6cd02d5dc82329b0cdb2e9423
pinkklss <shaker_04@mail.ru>
2024-11-21 22:22:56 +0700

    Update settings.py

--- UrbanDjango/UrbanDjango/settings.py
+++ UrbanDjango/UrbanDjango/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'task3',
+    'task2',
+    'UrbanDjango.templates',
 ]
 
 MIDDLEWARE = [
